0abfd485127f60c84c6d6ee4fb5edd0b21db947a
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-03-17 16:29:04 +0000

    feat: make issue title a clickable link to GitHub

--- src/App/View/Projects.purs
+++ src/App/View/Projects.purs
@@ -814,7 +814,23 @@ renderItemRow state projId mSf (ProjectItem item) =
                               _ -> []
                           else []
                         )
-                      <> [ HH.text item.title ]
+                      <> case item.url of
+                        Just url ->
+                          [ HH.a
+                              [ HP.href url
+                              , HP.target "_blank"
+                              , HP.class_
+                                  ( HH.ClassName
+                                      "detail-link"
+                                  )
+                              , HP.attr
+                                  (AttrName "onclick")
+                                  "event.stopPropagation()"
+                              ]
+                              [ HH.text item.title ]
+                          ]
+                        Nothing ->
+                          [ HH.text item.title ]
                   )
               , if null item.labels then
                   HH.text ""
